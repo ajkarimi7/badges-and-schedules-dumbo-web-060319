c49db61b13aba11cabda07f624d672c7c012bcbc
Ali Karimi <ajkarimi7@gmail.com>
2019-05-31 22:15:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator
+def batch_badge_creator(attendees)
   attendees.map do |attendee|
     "Hello, my name is #{attendee}."
   end 
